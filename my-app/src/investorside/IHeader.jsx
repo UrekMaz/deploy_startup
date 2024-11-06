fe231853550c79431a48fd7433b3e04a6f4d1c15
UrekMaz <dcmaureenmiranda@gmail.com>
2024-11-06 23:06:51 +0530

    p

--- my-app/src/investorside/IHeader.jsx
+++ my-app/src/investorside/IHeader.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
-import ProfileInfo from './ProfileInfo';
+import ProfileInfo from './Profileinfo';
 
 
 function IHeader() {
